48efab6439edc64b28b550e30d49f7ced97d2725
dksysd <dksysd@kumoh.ac.kr>
2025-05-13 11:56:21 +0900

    EF core Character Description 추가

---
+++ FightingGameServer_Rest/Migrations/20250513025541_AddDescriptionCharacter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FightingGameServer_Rest.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionCharacter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
